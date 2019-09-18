dc10b46f9f5d4e84a05c72242fdae0371db3be55
jumpt57 <julien.maupetit@protonmail.com>
2019-09-18 15:23:59 +0200

    improve sdk setup for windows and linux

--- src/test/java/com/github/jumpt57/endpoints/EndpointConfig.java
+++ src/test/java/com/github/jumpt57/endpoints/EndpointConfig.java
@@ -2,6 +2,7 @@ package com.github.jumpt57.endpoints;
 
 import com.google.appengine.tools.development.testing.BaseDevAppServerTestConfig;
 import com.google.common.io.ByteStreams;
+import lombok.NonNull;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -11,17 +12,36 @@ import java.util.List;
 
 public class EndpointConfig extends BaseDevAppServerTestConfig {
 
-    private static final String SDK_COMMAND = "gcloud.cmd info --format=\"value(installation.sdk_root)\"";
+    private static final boolean IS_WINDOWS = System.getProperty("os.name").toLowerCase().contains("windows");
+
+    private static final String SDK_COMMAND_WINDOWS = "gcloud.cmd info --format=\"value(installation.sdk_root)\"";
+    private static final String SDK_COMMAND_LINUX = "gcloud info --format=\"value(installation.sdk_root)\"";
+
+    private static final String SDK_JAVA_PATH_WINDOWS = "\\platform\\google_appengine\\google\\appengine\\tools\\java";
+    private static final String SDK_JAVA_PATH_LINUX = "/platform/google_appengine/google/appengine/tools/java";
+
     private static final String WEB_APP_PATH = "\\src\\main\\webapp";
-    private static final String SDK_JAVA_PATH = "\\platform\\google_appengine\\google\\appengine\\tools\\java";
 
     @Override
     public File getSdkRoot() {
         try {
-            Process runtime = Runtime.getRuntime().exec(SDK_COMMAND);
+            Process runtime = Runtime.getRuntime().exec(getSDKCommand());
+            runtime.waitFor();
+
             String baseSdkPath = new String(ByteStreams.toByteArray(runtime.getInputStream()));
             baseSdkPath = baseSdkPath.replace(System.lineSeparator(), "");
-            return new File(baseSdkPath + SDK_JAVA_PATH);
+
+            if (baseSdkPath.isEmpty()) {
+                throw new IllegalStateException("Gcloud was not found ! Try installing GCloud.");
+            }
+
+            File file = new File(getPath(baseSdkPath));
+
+            if (!file.exists()) {
+                throw new IllegalStateException("AppEngine Java SDK not found ! Try : gcloud components install app-engine-java");
+            }
+
+            return file;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -45,6 +65,26 @@ public class EndpointConfig extends BaseDevAppServerTestConfig {
             throw new RuntimeException(e);
         }
         return classPath;
+    }
+
+    private String[] getSDKCommand() {
+        if (IS_WINDOWS) {
+            return new String[]{
+                    "cmd.exe", "/C", SDK_COMMAND_WINDOWS
+            };
+        } else {
+            return new String[]{
+                    "/bin/bash", "-c", "-l", SDK_COMMAND_LINUX
+            };
+        }
+    }
+
+    private String getPath(@NonNull String basePath) {
+        if (IS_WINDOWS) {
+            return basePath + SDK_JAVA_PATH_WINDOWS;
+        } else {
+            return basePath + SDK_JAVA_PATH_LINUX;
+        }
     }
 
 }
